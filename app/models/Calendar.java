57127f8238b66f68b47ad7421283361c16901dab
Lukas Keller <lukas.keller@solnet.ch>
2011-10-13 16:59:58 +0200

    new database for Users
    	modified:   app/models/Calendar.java
    	new file:   app/models/UserDatabase.java

--- app/models/Calendar.java
+++ app/models/Calendar.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calendar {
-	
+
 	private List<Event> events;
 	private String name;
-	
-	public Calendar(String name){
+
+	public Calendar(String name) {
 		events = new ArrayList<Event>();
 		this.name = name;
 	}
 
-	
-	public void addEvent(Event e){
+	public void addEvent(Event e) {
 		events.add(e);
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public List<Event> getEvents() {
 		return events;
 	}
---
+++ app/models/UserDatabase.java
@@ -0,0 +1,31 @@
+package models;
+
+import java.util.ArrayList;
+
+public class UserDatabase {
+	private ArrayList<User> userList;
+	private static UserDatabase database;
+
+	private UserDatabase() {
+		this.userList = new ArrayList<User>();
+	}
+
+	public static UserDatabase getInstance() {
+		if (database == null) {
+			database = new UserDatabase();
+		}
+
+		return database;
+	}
+
+	public void createUser(String userName, String password) {
+		// TODO Check if a user with userName already exist and throw an
+		// exception
+		User user = new User(userName, password);
+		userList.add(user);
+	}
+
+	public void deleteUser(String userName, String password) {
+		// TODO
+	}
+}
