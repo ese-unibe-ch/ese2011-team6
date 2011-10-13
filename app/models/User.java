ea18286160d5ecce6e46ffb6a367a13b8dc43c26
Lukas Keller <lukas.keller@solnet.ch>
2011-10-13 14:51:37 +0200

    Create a simple user class
    *user has a list of friends
    *user can have more than one calendar
    	modified:   .classpath
    	new file:   app/models/User.java

---
+++ app/models/User.java
@@ -0,0 +1,37 @@
+package models;
+
+import java.util.ArrayList;
+
+public class User 
+{
+	private ArrayList<Calendar> calendarList;
+	private String userName;
+	private String password;
+	
+	private ArrayList<User> friendList;
+	
+	public User(String userName, String password)
+	{
+		this.userName=userName;
+		this.password=password;
+		
+		this.calendarList=new ArrayList<Calendar>();
+		this.friendList=new ArrayList<User>();
+	}
+	
+	public ArrayList<User> getFriendList()
+	{
+		return this.getFriendList();
+	}
+	
+	public ArrayList<Calendar> getCalendars()
+	{
+		return this.calendarList;
+	}
+	
+	public String getName()
+	{
+		return this.userName;
+	}
+
+}
