2375f989384f5cb6f86fd54c4e6f3f3952ca325f
lyriael <lyriael@gmx.net>
2011-10-13 14:44:59 +0200

    Created Class Event with variables and get-/setters

--- app/models/Event.java
+++ app/models/Event.java
@@ -1,6 +1,46 @@
 package models;
 
-public class Event 
-{
+import java.util.Date;
+
+public class Event {
+	
+	private Date start;
+	private Date end;
+	private boolean isPublic;
+	private String name;
+	
+	public Event(String name, boolean isPublic, Date start, Date end){
+		//TODO throw exception if input is not valid ?
+		this.name = name;
+		this.isPublic = isPublic;
+		this.start = start;
+		this.end = end;
+	}
+	
+	public void setStart(Date start) {
+		this.start = start;
+	}
+	public Date getStart() {
+		return start;
+	}
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+	public Date getEnd() {
+		return end;
+	}
+	public void setPublic(boolean isPublic) {
+		this.isPublic = isPublic;
+	}
+	public boolean isPublic() {
+		return isPublic;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	
 
 }
