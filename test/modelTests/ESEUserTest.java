8e4d280ba7ae26c8a15b32bf7227c671722fc672
lyriael <lyriael@gmx.net>
2011-10-22 20:34:06 +0200

    More tests for ESEUser: adding & removing Calendars
    @Model-team
    how can you get a calendar from the list?
    Should it not be possible to retrieve the calendar by its name?

--- test/modelTests/ESEUserTest.java
+++ test/modelTests/ESEUserTest.java
@@ -2,7 +2,10 @@ package modelTests;
 
 import java.util.List;
 
+import models.ESECalendar;
+import models.ESEFactory;
 import models.ESEUser;
+import net.sf.oval.constraint.AssertNullCheck;
 
 import org.junit.Test;
 
@@ -38,6 +41,11 @@ public class ESEUserTest extends UnitTest {
 		assertEquals("secret", bob.password);
 		assertEquals("Bob", bob.firstName);
 		assertEquals("Bobber", bob.familyName);
+		
+		//check rest of initialization
+		//TODO is that a problem? Does this problem only occure when date from .yml file is taken?
+		// else the constructor would be used and the lists should be created, right?
+		assertNotNull(bob.calendarList);
 	}
 	
 	
@@ -65,6 +73,7 @@ public class ESEUserTest extends UnitTest {
 		assertTrue(user.familyName.equals("Müller"));
 		assertTrue(user.firstName.equals("Hans"));
 		assertTrue(user.password.equals("sehrgeheim"));
+		
 	}
 	
 	@Test
@@ -74,18 +83,41 @@ public class ESEUserTest extends UnitTest {
 		hansi.createCalendar("Hausaufgaben");
 		
 		assertNotNull(hansi.calendarList.get(0));
-		//test with database user
-		//TODO find out, if it can/should work
-/*		ESEUser bob = ESEUser.find("byUsername", "bob").first();
-		assertNotNull(bob);
+		assertEquals("Hausaufgaben", hansi.calendarList.get(0).name);
 		
-		bob.createCalendar("ShoppingListe");
-		assertNotNull(bob.calendarList);
+		hansi.createCalendar("Shopping Liste");
+		assertEquals(2, hansi.calendarList.size());
+		
+		//TODO find out, if it can/should work
+		//test with database/existing user
+/*		ESEUser bob = ESEUser.find("byUsername", "bob").first();
+		ESECalendar cal = new ESECalendar("AndereShoppingListe");
+		assertNotNull(cal);
+		bob.calendarList.add(cal);
 */	}
 	
 	@Test
 	public void shouldRemoveCalendar(){
-		//TODO
+		//set up
+		ESEUser hansi = new ESEUser("hansi", "sehrgeheim");
+		hansi.createCalendar("Hausaufgaben");
+		hansi.createCalendar("Shopping Liste");
+		//TODO find better way to get calendars of user!
+		ESECalendar husi = hansi.calendarList.get(0);
+		ESECalendar shop = hansi.calendarList.get(1);
+		
+		//remove one
+		hansi.removeCalendar(husi.id);
+		assertEquals(1, hansi.calendarList.size());
+		assertEquals(shop, hansi.calendarList.get(0));
+		
+		//remove last
+		hansi.removeCalendar(shop.id);
+		assertEquals(0, hansi.calendarList.size());
+		
+		//not tested it the case, when the list is empty or
+		//when the id is wrong. This is not necessairy because
+		//it is handled within the controller.
 	}
 	
 	@Test
