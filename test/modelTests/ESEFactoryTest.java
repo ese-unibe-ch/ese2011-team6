54d144b087db4224f5381649a7d1d193ab28a5e2
Rafael Breu <rafael.breu@students.unibe.ch>
2011-10-23 15:41:19 +0200

    Expanded group and calendar tests

---
+++ test/modelTests/ESEFactoryTest.java
@@ -0,0 +1,31 @@
+package modelTests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import models.ESECalendar;
+import models.ESEEvent;
+import models.ESEFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.test.Fixtures;
+import play.test.UnitTest;
+
+
+public class ESEFactoryTest extends UnitTest{
+		
+	@Before
+	public void setUp() {
+		
+	}
+	
+	//TODO: Everything
+}
