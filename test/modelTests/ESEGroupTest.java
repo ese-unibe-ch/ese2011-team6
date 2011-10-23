aa652d2326eb46eb7010ff1a5246554bde4ebaab
Rafael Breu <rafael.breu@students.unibe.ch>
2011-10-23 15:02:26 +0200

    Added user "bill" to data.yml, expanded ESEGroupTest

--- test/modelTests/ESEGroupTest.java
+++ test/modelTests/ESEGroupTest.java
@@ -8,6 +8,7 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 import models.ESEEvent;
+import models.ESEFactory;
 import models.ESEGroup;
 import models.ESEUser;
 
@@ -15,6 +16,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import play.data.validation.Required;
+import play.test.Fixtures;
 import play.test.UnitTest;
 
 /**
@@ -24,7 +26,7 @@ import play.test.UnitTest;
 public class ESEGroupTest extends UnitTest{
 	
 	private ESEGroup testGroup;
-
+	
 	@Before
 	public void setUp() {
 		this.testGroup = new ESEGroup("TestGroupName1");
@@ -42,28 +44,89 @@ public class ESEGroupTest extends UnitTest{
 		
 		//TODO: What happens if empty string? Will this be handled in the model or in the view?
 	}
-	/*
+	
 	@Test
 	public void shouldAddUsers(){
-		//TODO
+		
+		Fixtures.loadModels("data.yml");
+		assertEquals(testGroup.userList.size(),0);
+		this.testGroup.addUser("bob");
+		assertEquals(testGroup.userList.size(),1);
+		this.testGroup.addUser("bill");
+		assertEquals(testGroup.userList.size(),2);
 	}
 	
-	@Given("shouldAddUsers")
+	@Test
+	public void shouldOnlyAddExistingUsers(){
+		
+		//TODO: This test fails, because a null object is added, if a username
+		// that does not exist is sent to #addUser. - How is this problem handled?
+		// Is the view responsible that no non-existing users are sent to
+		// addUser, or is addUser responsible that non-existing users are ignored?
+		
+		Fixtures.deleteDatabase();
+		Fixtures.loadModels("data.yml");
+		assertEquals(testGroup.userList.size(),0);
+		this.testGroup.addUser("nouser");
+		assertEquals(testGroup.userList.size(),0);
+		this.testGroup.addUser("bill");
+		assertEquals(testGroup.userList.size(),1);
+	}
+	
+	@Test
+	public void shouldNotAddUserIfAlreadyInList(){
+		
+		assertEquals(testGroup.userList.size(),0);
+		this.testGroup.addUser("bob");
+		assertEquals(testGroup.userList.size(),1);
+		this.testGroup.addUser("bob");
+		assertEquals(testGroup.userList.size(),1);
+	}
+	
+	@Test
 	public void shouldReturnListOfAllUsers(){
 		
-		//TODO
+		Fixtures.deleteDatabase();
+		Fixtures.loadModels("data.yml");
+		this.testGroup.addUser("bob");
+		this.testGroup.addUser("bill");
+		
+		assertTrue(testGroup.getAllUser().size()==2);
+		assertTrue(testGroup.getAllUser().get(0).username.equals("bob"));
+		assertTrue(testGroup.getAllUser().get(1).username.equals("bill"));
+		
 	}
 	
-	@Given("shouldAddUsers")
+	@Test
 	public void shouldRemoveCorrectUser(){
 		
-		//TODO
+		Fixtures.deleteDatabase();
+		Fixtures.loadModels("data.yml");
+		this.testGroup.addUser("bob");
+		this.testGroup.addUser("bill");
+		
+		assertTrue(testGroup.getAllUser().size()==2);
+		
+		this.testGroup.removeUser("bob");
+		
+		assertTrue(testGroup.getAllUser().size()==1);
+		
+		assertTrue(testGroup.getAllUser().get(0).username.equals("bill"));
+		assertTrue(testGroup.isUserInGroup("bill"));
+		assertFalse(testGroup.isUserInGroup("bob"));
+		assertFalse(testGroup.isUserInGroup("nouser"));
+		
 	}
 	
-	@Given("shouldAddUsers")
+	@Test
 	public void shouldReturnWheterUserIsInList(){
+		Fixtures.deleteDatabase();
+		Fixtures.loadModels("data.yml");
+		this.testGroup.addUser("bob");
 		
-		//TODO
-	}*/
+		assertTrue(testGroup.isUserInGroup("bob"));
+		assertFalse(testGroup.isUserInGroup("bill"));
+		
+	}
 	
 }
