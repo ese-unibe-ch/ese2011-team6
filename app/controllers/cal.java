501ddfab63cfcd4f68b201ab8c160bebaedd714a
Petar Bogdanovic <petar@smokva.net>
2011-10-26 00:58:39 +0200

    implement event creation

--- app/controllers/cal.java
+++ app/controllers/cal.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import java.util.List;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import play.mvc.*;
+import play.data.validation.*;
 import models.*;
 
 @With(Secure.class)
@@ -19,5 +22,42 @@ public class cal extends Controller
 				.fetch();
 		}
 		render(le);
+	}
+
+	public static void add_evt (
+		String id
+	) {
+		render(id);
+	}
+
+	public static void add_evt_post (
+		String id,
+		@Required String name,
+		@Required String beg,
+		@Required String end,
+		String pub
+	) {
+		Date dbeg, dend;
+		Boolean err_date = false;
+		ESECalendar c = ESECalendar.getCalendar(id);
+		SimpleDateFormat sdf = new SimpleDateFormat(
+			"dd.MM.yyyy HH:mm");
+
+		try {
+			dbeg = sdf.parse(beg);
+			dend = sdf.parse(end);
+			if (dend.compareTo(dbeg) < 0) {
+				throw new Exception();
+			}
+		} catch (Exception e) {
+			err_date = true;
+		}
+		if (!validation.hasErrors() && !err_date && c != null) {
+			pub = pub==null ?"false" :"true";
+			ESEFactory.createEvent(name, beg, end, pub, c);
+		}
+		params.flash();
+		validation.keep();
+		add_evt(id);
 	}
 }
