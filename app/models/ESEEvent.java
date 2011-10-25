92d20f8139f7954585671ec3047fce9fe37d536a
Renato Corti <renato.corti@students.unibe.ch>
2011-10-25 23:45:54 +0200

    Add missing save() methods
    Beautify checkEventOverlaps() helper methods
    Replenish .gitignore
    Renew constructors
    
    	modified:   .gitignore
    	modified:   app/models/ESECalendar.java
    	modified:   app/models/ESEConversionHelper.java
    	modified:   app/models/ESEEvent.java
    	modified:   app/models/ESEFactory.java
    	deleted:    test/data.yml~

--- app/models/ESEEvent.java
+++ app/models/ESEEvent.java
@@ -1,15 +1,13 @@
 package models;
 
 import java.util.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-
+import javax.persistence.*;
 import play.data.validation.Required;
 import play.db.jpa.Model;
 
 @Entity
-public class ESEEvent extends Model {
+public class ESEEvent extends Model
+{
 
 	@ManyToOne
 	public ESECalendar correspondingCalendar;
@@ -18,58 +16,69 @@ public class ESEEvent extends Model {
 	public Date endDate;
 	public boolean isPublic;
 
-	public ESEEvent(@Required String eventName, @Required String strStart,
-			@Required String strEnd, @Required String strIsPublic,
-			ESECalendar correspondingCalendar) {
+	public ESEEvent(@Required String eventName,
+			@Required String strStart, @Required String strEnd,
+			@Required ESECalendar correspondingCalendar, @Required String strIsPublic)
+	{
 		this.eventName = eventName;
+		// TODO: Is strStart <= strEnd?
 		this.startDate = ESEConversionHelper.convertStringToDate(strStart);
 		this.endDate = ESEConversionHelper.convertStringToDate(strEnd);
 		this.correspondingCalendar = correspondingCalendar;
+		this.correspondingCalendar.save();
 		this.isPublic = Boolean.parseBoolean(strIsPublic);
 	}
 
 	/**
-	 * @deprecated Es soll stattdessen {@link #editEventName(String)} verwendet
-	 *             werden
+	 * @deprecated Es soll stattdessen {@link #editEventName(String)} verwendet werden
 	 */
-	public void renameEvent(@Required String newName) {
+	public void renameEvent(@Required String newName)
+	{
 		editEventName(newName);
 	}
 
-	public String getEventName() {
+	public String getEventName()
+	{
 		return this.eventName;
 	}
 
-	public Date getStartDate() {
+	public Date getStartDate()
+	{
 		return this.startDate;
 	}
 
-	public Date getEndDate() {
+	public Date getEndDate()
+	{
 		return this.endDate;
 	}
 
-	public boolean isPublic() {
+	public boolean isPublic()
+	{
 		return this.isPublic;
 	}
 
 	// TODO: Setters = editEvents? How does this look like?
-	public void editEventName(@Required String newName) {
+	public void editEventName(@Required String newName)
+	{
 		this.eventName = newName;
+		this.save();
 	}
 
-	public void editStartDate(@Required String newStartDate) {
+	public void editStartDate(@Required String newStartDate)
+	{
 		this.startDate = ESEConversionHelper.convertStringToDate(newStartDate);
+		this.save();
 	}
 
-	public void editEndDate(@Required String newEndDate) {
+	public void editEndDate(@Required String newEndDate)
+	{
 		this.endDate = ESEConversionHelper.convertStringToDate(newEndDate);
+		this.save();
 	}
 
-	public void editVisibility(@Required String publicViewable) {
-		this.isPublic = Boolean.parseBoolean(publicViewable);
-	}
-
-	public String getName() {
-		return this.eventName;
+	public void editVisibility(@Required String publiclyViewable)
+	{
+		this.isPublic = Boolean.parseBoolean(publiclyViewable);
+		this.save();
 	}
 }
--- app/models/ESEFactory.java
+++ app/models/ESEFactory.java
@@ -7,8 +7,11 @@ package models;
  * @author lukas
  * 
  */
-public abstract class ESEFactory {
-	public static ESECalendar createCalendar(String calendarName, ESEUser owner) {
+public abstract class ESEFactory
+{
+	public static ESECalendar createCalendar(String calendarName, ESEUser owner)
+	{
+		//TODO: Calendar must be unique
 		ESECalendar calendar = new ESECalendar(calendarName, owner);
 		calendar.save();
 
@@ -16,31 +19,36 @@ public abstract class ESEFactory {
 	}
 
 	public static ESEEvent createEvent(String eventName, String strStart,
-			String strEnd, String strIsPublic, ESECalendar correspondingCalendar) {
-		ESEEvent event = new ESEEvent(eventName, strStart, strEnd, strIsPublic,
-				correspondingCalendar);
-		// event.save();
+			String strEnd, ESECalendar correspondingCalendar, String strIsPublic)
+	{
+		ESEEvent event = new ESEEvent(eventName, strStart, strEnd,
+				correspondingCalendar, strIsPublic);
+		correspondingCalendar.save();
+		event.save();
 
 		return event;
 	}
 
-	public static ESEGroup createGroup(String groupName, ESEUser owner) {
+	public static ESEGroup createGroup(String groupName, ESEUser owner)
+	{
 		ESEGroup group = new ESEGroup(groupName, owner);
 		// group.save();
 
 		return group;
 	}
 
-	public static ESEUser createUser(String username, String password,
-			String firstName, String familyName) {
-		ESEUser user = new ESEUser(username, password, firstName, familyName);
+	public static ESEUser createUser(String username, String password)
+	{
+		ESEUser user = new ESEUser(username, password);
 		user.save();
 
 		return user;
 	}
 
-	public static ESEUser createUser(String username, String password) {
-		ESEUser user = new ESEUser(username, password);
+	public static ESEUser createUser(String username, String password,
+			String firstName, String familyName)
+	{
+		ESEUser user = new ESEUser(username, password, firstName, familyName);
 		user.save();
 
 		return user;
