dcf7426a8f805297757640115177090d2f92192d
Lukas Keller <lukas.keller@solnet.ch>
2011-10-25 14:35:31 +0200

    ESEEvent:
    * fix rename ConversionHelper to ESEConversionHelper
    * fix constructor
    
    	modified:   app/models/ESEEvent.java

--- app/models/ESEEvent.java
+++ app/models/ESEEvent.java
@@ -1,15 +1,15 @@
 package models;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
 import play.data.validation.Required;
 import play.db.jpa.Model;
 
 @Entity
-public class ESEEvent extends Model
-{
+public class ESEEvent extends Model {
 
 	@ManyToOne
 	public ESECalendar correspondingCalendar;
@@ -20,60 +20,51 @@ public class ESEEvent extends Model
 
 	public ESEEvent(@Required String name, @Required String strStart,
 			@Required String strEnd, @Required String strIsPublic) {
-	{
 		this.eventName = eventName;
-		this.startDate = ConversionHelper.convertStringToDate(strStart);
-		this.endDate = ConversionHelper.convertStringToDate(strEnd);
+		this.startDate = ESEConversionHelper.convertStringToDate(strStart);
+		this.endDate = ESEConversionHelper.convertStringToDate(strEnd);
 		this.correspondingCalendar = correspondingCalendar;
 		this.isPublic = Boolean.parseBoolean(strIsPublic);
 	}
 
 	/**
-	 * @deprecated Es soll stattdessen {@link #editEventName(String)} verwendet werden
+	 * @deprecated Es soll stattdessen {@link #editEventName(String)} verwendet
+	 *             werden
 	 */
-	public void renameEvent(@Required String newName)
-	{
+	public void renameEvent(@Required String newName) {
 		editEventName(newName);
 	}
 
-	public String getEventName()
-	{
+	public String getEventName() {
 		return this.eventName;
 	}
 
-	public Date getStartDate()
-	{
+	public Date getStartDate() {
 		return this.startDate;
 	}
 
-	public Date getEndDate()
-	{
+	public Date getEndDate() {
 		return this.endDate;
 	}
 
-	public boolean isPublic()
-	{
+	public boolean isPublic() {
 		return this.isPublic;
 	}
 
-	//TODO: Setters = editEvents? How does this look like?
-	public void editEventName(@Required String newName)
-	{
+	// TODO: Setters = editEvents? How does this look like?
+	public void editEventName(@Required String newName) {
 		this.eventName = newName;
 	}
 
-	public void editStartDate(@Required String newStartDate)
-	{
-		this.startDate = ConversionHelper.convertStringToDate(newStartDate);
+	public void editStartDate(@Required String newStartDate) {
+		this.startDate = ESEConversionHelper.convertStringToDate(newStartDate);
 	}
 
-	public void editEndDate(@Required String newEndDate)
-	{
-		this.endDate = ConversionHelper.convertStringToDate(newEndDate);
+	public void editEndDate(@Required String newEndDate) {
+		this.endDate = ESEConversionHelper.convertStringToDate(newEndDate);
 	}
 
-	public void editVisibility(@Required String publicViewable)
-	{
+	public void editVisibility(@Required String publicViewable) {
 		this.isPublic = Boolean.parseBoolean(publicViewable);
 	}
 
