31e086202456cce8191b0f831cc03d8f4459bb2e
Petar Bogdanovic <petar@smokva.net>
2011-10-26 04:11:48 +0200

    implement calendar display, add colors

--- app/controllers/cal.java
+++ app/controllers/cal.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.util.List;
 import java.util.Date;
+import java.util.Calendar;
 import java.text.SimpleDateFormat;
 import play.mvc.*;
 import play.data.validation.*;
@@ -10,18 +11,53 @@ import models.*;
 @With(Secure.class)
 public class cal extends Controller
 {
+	public static int[] monthdays = {
+		31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public static String[] monthnames = {
+		"January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November",
+		"December" };
+
 	public static void ls_evts (
-		String id
+		String id,
+		String year,
+		String month,
+		String day
 	) {
 		ESECalendar c;
 		List<ESEEvent> le = null;
+		int y, m, d, cy, cm, cd, days;
+		String mname;
 
 		if ((c = ESECalendar.getCalendar(id)) != null) {
 			le = permitted(c)
 				?c.getAllEventsAsList()
 				:c.getPublicEventsAsList();
 		}
-		render(id, le);
+
+		if (year == null || month == null || day == null) {
+			y = cyear();
+			m = cmonth();
+			d = cday();
+		}
+		else {
+			y = Integer.parseInt(year);
+			m = Integer.parseInt(month);
+			d = Integer.parseInt(day);
+		}
+		cy = cyear();
+		cm = cmonth();
+		cd = cday();
+		days = monthdays[m-1];
+		mname = monthnames[m-1];
+		render(id, le, y, m, d, cy, cm, cd, days, mname);
+	}
+
+	public static void ls_evts_cur (
+		String id
+	) {
+		cal.ls_evts(id, null, null, null);
 	}
 
 	public static void add_evt (
@@ -58,7 +94,7 @@ public class cal extends Controller
 				ESEFactory.createEvent(
 					name, beg, end, pub, c);
 			}
-			cal.ls_evts(id);
+			cal.ls_evts_cur(id);
 		}
 		params.flash();
 		validation.keep();
@@ -73,7 +109,7 @@ public class cal extends Controller
 		if (c != null && permitted(c)) {
 			c.removeEvent(Long.parseLong(eid));
 		}
-		cal.ls_evts(id);
+		cal.ls_evts_cur(id);
 	}
 
 	public static Boolean permitted (
@@ -82,5 +118,23 @@ public class cal extends Controller
 		String authid = Secure.Security.connected();
 		String cal_owner = c.getOwner().getUsername();
 		return authid.equals(cal_owner);
+	}
+
+	public static int cyear (
+	) {
+		Calendar c = Calendar.getInstance();
+		return c.get(Calendar.YEAR);
+	}
+
+	public static int cmonth (
+	) {
+		Calendar c = Calendar.getInstance();
+		return c.get(Calendar.MONTH);
+	}
+
+	public static int cday (
+	) {
+		Calendar c = Calendar.getInstance();
+		return c.get(Calendar.DAY_OF_MONTH);
 	}
 }
