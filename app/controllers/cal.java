2f99ecc0f12b1cae522e52ae17c272bf6b6bedfa
Petar Bogdanovic <petar@smokva.net>
2011-10-27 23:45:50 +0200

    implement days of week

--- app/controllers/cal.java
+++ app/controllers/cal.java
@@ -19,6 +19,10 @@ public class cal extends Controller
 		"July", "August", "September", "October", "November",
 		"December" };
 
+	public static String[] daysofweek = {	/* see dayofweek() */
+		"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+		"Friday", "Saturday" };
+
 	public static void ls_evts (
 		String id,
 		String year,
@@ -29,6 +33,7 @@ public class cal extends Controller
 		List<ESEEvent> le = null;
 		int y, m, d, cy, cm, cd, days;
 		String mname;
+		String[] dow;
 
 		if ((c = ESECalendar.getCalendar(id)) != null) {
 			le = permitted(c)
@@ -51,7 +56,8 @@ public class cal extends Controller
 		cd = cday();
 		days = monthdays[m-1];
 		mname = monthnames[m-1];
-		render(id, le, y, m, d, cy, cm, cd, days, mname);
+		dow = daysofweek_to_dow(daysofweek, y, m, d);
+		render(id, le, y, m, d, cy, cm, cd, days, mname, dow);
 	}
 
 	public static void ls_evts (
@@ -136,5 +142,37 @@ public class cal extends Controller
 	) {
 		Calendar c = Calendar.getInstance();
 		return c.get(Calendar.DAY_OF_MONTH);
+	}
+
+	/**
+	 *	derived from http://users.aol.com/s6sj7gt/mikecal.htm
+	 *	output: 0 (sun) -> 6 (sat)
+	 */
+	public static int dayofweek (
+		int y, int m, int d
+	) {
+		int z = 0;
+		int adj = 0;
+
+		if (m < 3) {
+			z = y-1;
+		}
+		else {
+			z = y;
+			adj = 2;
+		}
+		return ((int)(23*m/9)+d+4+y+(int)(z/4)-(int)(z/100)+(int)(z/400)-adj)%7;
+	}
+
+	public static String[] daysofweek_to_dow (
+		String[] daysofweek,
+		int y, int m, int d
+	) {
+		String[] r = new String[7];
+		int dow = dayofweek(y, m, d);
+		for (int i=0; i<7; i++) {
+			r[i] = daysofweek[(dow+i)%7];
+		}
+		return r;
 	}
 }
