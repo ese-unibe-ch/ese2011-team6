7de74be638dbd707cef79686426b38cdf0db54e0
Rafael Breu <rafael.breu@students.unibe.ch>
2011-10-29 23:21:29 +0200

    Added JavaDoc

--- app/models/ESECalendar.java
+++ app/models/ESECalendar.java
@@ -8,12 +8,30 @@ import play.data.validation.Required;
 import play.db.jpa.JPABase;
 import play.db.jpa.Model;
 
+/**
+ * Every  ESECalendar has a name, an owner and a
+ * list of{@link ESEEvent}s.
+ * This class has functionalities to add/remove events and
+ * provides lists of events at certain days, of certain 
+ * visibility etc.
+ * 
+ * @see ESEEvent
+ */
 @Entity
 public class ESECalendar extends Model
 {
+	/**
+	 * Name of this ESECalendar
+	 */
 	public String calendarName;
+	/**
+	 * List of ESEEvents in this ESECalendar
+	 */
 	@OneToMany(mappedBy = "correspondingCalendar", cascade = CascadeType.ALL)
 	public List<ESEEvent> eventList;
+	/**
+	 * Owner ({@link ESEUser}) of this ESECalendar
+	 */
 	@ManyToOne
 	public ESEUser owner;
 
@@ -24,6 +42,14 @@ public class ESECalendar extends Model
 		this.owner = owner;
 	}
 
+	/**
+	 * Adds an {@link ESEEvent} to this ESECalendar
+	 * 
+	 * @param eventName
+	 * @param startDate
+	 * @param endDate
+	 * @param isPublic
+	 */
 	public void addEvent(@Required String eventName, @Required String startDate,
 						 @Required String endDate, @Required String isPublic)
 	{
@@ -82,6 +108,12 @@ public class ESECalendar extends Model
 		return newStartTime <= existingStartTime && existingEndTime <= newEndTime;
 	}
 
+	/**
+	 * Returns this ESECalendar
+	 * 
+	 * @param id
+	 * @return this  ESECalendar
+	 */
 	public static ESECalendar getCalendar(String id) {
 		long cid = Long.parseLong(id);
 		return findById(cid);
@@ -95,22 +127,39 @@ public class ESECalendar extends Model
 		return this.getCalendarName();
 	}
 
+	/**
+	 * Returns the name of this  ESECalendar
+	 * @return name of ESECalendar
+	 */
 	public String getCalendarName()
 	{
 		return this.calendarName;
 	}
 
+	 /**
+	  * Returns the owner ({@link ESEUser}) of this ESECalendar
+	  * @return the owner of this ESECalendar
+	  */
 	public ESEUser getOwner()
 	{
 		return this.owner;
 	}
 
+	/**
+	 * Renames this ESECalendar
+	 * @param newName
+	 */
 	public void renameCalendar(@Required String newName)
 	{
 		this.calendarName = newName;
 		this.save();
 	}
 
+	/**
+	 * Looks for an {@link ESEEvent} with a certain name and
+	 * removes it from this calendar
+	 * @param eventName
+	 */
 	public void removeEvent(@Required String eventName)
 	{
 		for (ESEEvent e : this.eventList)
@@ -126,11 +175,25 @@ public class ESECalendar extends Model
 		//TODO: Complain as this event is not in the list
 	}
 
+	/**
+	 * Removes an {@link ESEEvent} by its unique id
+	 * @param eventId
+	 */
 	public void removeEvent(@Required Long eventId)
 	{
 		this.removeEvent(((ESEEvent) ESEEvent.findById(eventId)).getEventName());
 	}
 
+	/**
+	 * Returns a list of all {@link ESEEvent}s at a certain day
+	 * which have the visibility "public" (i.e. can be seen by
+	 * every {@link ESEUser}, not only by their owners.
+	 * 
+	 * @param calendarDay
+	 * @return list of all public events at a certain day
+	 * @see ESEEvent
+	 * @see ESEUser
+	 */
 	public ArrayList<ESEEvent> getListOfPubEventsRunningAtDay(@Required String calendarDay)
 	{
 		ArrayList<ESEEvent> eventsFormDate = new ArrayList<ESEEvent>();
@@ -153,6 +216,13 @@ public class ESECalendar extends Model
 		return eventsFormDate;
 	}
 
+	/**
+	 * Returns a list of all {@link ESEEvent}s at a certain day
+	 * 
+	 * @param calendarDay
+	 * @return list of all events at a certain day
+	 * @see ESEEvent
+	 */
 	public ArrayList<ESEEvent> getListOfEventsRunningAtDay(@Required String calendarDay)
 	{
 		ArrayList<ESEEvent> eventsFormDate = new ArrayList<ESEEvent>();
@@ -172,16 +242,36 @@ public class ESECalendar extends Model
 		return eventsFormDate;
 	}
 
+	/**
+	 * Returns an iterator of all {@link ESEEvent}s at a certain day
+	 * 
+	 * @param calendarDay
+	 * @return list of all events at a certain day
+	 * @see ESEEvent
+	 */
 	public Iterator<ESEEvent> getIteratorOfEventsRunningAtDay(@Required String calendarDay)
 	{
 		return this.getListOfEventsRunningAtDay(calendarDay).iterator();
 	}
 
+	/**
+	 * Returns an ArrayList of all {@link ESEEvent}s in this ESECalendar
+	 * @return all events in this calendar
+	 */
 	public ArrayList<ESEEvent> getAllEventsAsList()
 	{
 		return new ArrayList<ESEEvent>(eventList);
 	}
 
+	/**
+	 * Returns an ArrayList of all {@link ESEEvent}s in this ESECalendar
+	 * which have the visibility "public" (i.e. can be seen by
+	 * every {@link ESEUser}, not only by their owners.
+	 * 
+	 * @return a list of all public events
+	 * @see ESEEvent
+	 * @see ESEUser
+	 */
 	public ArrayList<ESEEvent> getPublicEventsAsList()
 	{
 		ArrayList<ESEEvent> publicEventList = new ArrayList<ESEEvent>();
@@ -195,12 +285,25 @@ public class ESECalendar extends Model
 
 		return publicEventList;
 	}
-
+	
+	/**
+	 * Returns an Iterator of all {@link ESEEvent}s in this ESECalendar
+	 * @return all events in this calendar
+	 */
 	public Iterator<ESEEvent> getAllEventsAsIterator()
 	{
 		return this.getPublicEventsAsList().iterator();
 	}
 
+	/**
+	 * Returns an Iterator of all {@link ESEEvent}s in this ESECalendar
+	 * which have the visibility "public" (i.e. can be seen by
+	 * every {@link ESEUser}, not only by their owners.
+	 * 
+	 * @return a list of all public events
+	 * @see ESEEvent
+	 * @see ESEUser
+	 */
 	public Iterator<ESEEvent> getPublicEventsAsIterator()
 	{
 		return this.getAllEventsAsList().iterator();
